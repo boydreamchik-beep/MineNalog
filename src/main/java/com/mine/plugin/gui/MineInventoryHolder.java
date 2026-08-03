7650d741baca1a35510feefa5cbfbfd67eb15d91
boydreamchik-beep <boy.dreamchik@gmail.com>
2026-08-03 18:19:56 +0300

    Enhance MineInventoryHolder with new GUI types
    Added new GUI types SHOP and KAZNA_PAGED, and updated the constructor to include a page parameter.

--- src/main/java/com/mine/plugin/gui/MineInventoryHolder.java
+++ src/main/java/com/mine/plugin/gui/MineInventoryHolder.java
@@ -5,27 +5,36 @@ import org.bukkit.inventory.InventoryHolder;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Маркерный InventoryHolder для идентификации наших GUI.
- * 
- * ИЗМЕНЕНИЕ: Добавлен тип ALREADY_IN_MINE для окна
- * "вы уже в шахте"
+ * ИЗМЕНЕНИЕ: Добавлены типы SHOP и KAZNA_PAGED
  */
 public class MineInventoryHolder implements InventoryHolder {
 
     public enum GUIType {
-        MINE_LEVEL_SELECT,     // Выбор уровня шахты
-        KAZNA_VIEW,            // Просмотр казны
-        ALREADY_IN_MINE        // ➕ НОВЫЙ: Окно "вы уже в шахте"
+        MINE_LEVEL_SELECT,
+        KAZNA_VIEW,
+        ALREADY_IN_MINE,
+        SHOP,              // ➕ НОВЫЙ
+        KAZNA_PAGED        // ➕ НОВЫЙ: казна с пагинацией
     }
 
     private final GUIType type;
+    private int page = 0;  // ➕ НОВЫЙ: номер страницы для казны
 
     public MineInventoryHolder(GUIType type) {
         this.type = type;
     }
 
+    public MineInventoryHolder(GUIType type, int page) {
+        this.type = type;
+        this.page = page;
+    }
+
     public GUIType getType() {
         return type;
+    }
+
+    public int getPage() {
+        return page;
     }
 
     @Override
