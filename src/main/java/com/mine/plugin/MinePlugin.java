fd2fc8c01e189831b8b6033f546ff787f5443627
boydreamchik-beep <boy.dreamchik@gmail.com>
2026-08-04 10:39:06 +0300

    Refactor imports and add reload command

--- src/main/java/com/mine/plugin/MinePlugin.java
+++ src/main/java/com/mine/plugin/MinePlugin.java
@@ -1,40 +1,30 @@
 package com.mine.plugin;
 
-import com.mine.plugin.commands.CreditCommand;
-import com.mine.plugin.commands.KaznaCommand;
-import com.mine.plugin.commands.PassportCommand;
-import com.mine.plugin.commands.ShopCommand;
+import com.mine.plugin.commands.*;
 import com.mine.plugin.gui.MineLevelGUI;
-import com.mine.plugin.listeners.CompassListener;
-import com.mine.plugin.listeners.MineBlockBreakListener;
-import com.mine.plugin.listeners.MineEntryListener;
-import com.mine.plugin.managers.CreditManager;
-import com.mine.plugin.managers.FreezeManager;
-import com.mine.plugin.managers.KaznaManager;
-import com.mine.plugin.managers.MineGenerator;
-import com.mine.plugin.managers.PassportManager;
-import com.mine.plugin.managers.ScoreboardManager;
-import com.mine.plugin.managers.TaxTracker;
+import com.mine.plugin.listeners.*;
+import com.mine.plugin.managers.*;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-/**
- * ИЗМЕНЕНИЯ:
- * - Добавлен CreditManager
- * - Добавлен PassportManager
- * - Добавлены команды /credit и /passport
- */
 public class MinePlugin extends JavaPlugin {
 
     private static MinePlugin instance;
+    private ConfigManager configManager;
     private KaznaManager kaznaManager;
     private MineLevelGUI mineLevelGUI;
     private TaxTracker taxTracker;
     private FreezeManager freezeManager;
     private MineGenerator mineGenerator;
     private ScoreboardManager scoreboardManager;
-    private CreditManager creditManager;       // ➕ НОВОЕ
-    private PassportManager passportManager;   // ➕ НОВОЕ
+    private CreditManager creditManager;
+    private PassportManager passportManager;
 
     @Override
     public void onEnable() {
@@ -44,15 +34,19 @@ public class MinePlugin extends JavaPlugin {
             getDataFolder().mkdirs();
         }
 
-        // Менеджеры
+        // Конфиг (первым!)
+        configManager = new ConfigManager(this);
+        configManager.load();
+
+        // Менеджеры данных
         kaznaManager = new KaznaManager(this);
         kaznaManager.load();
 
-        creditManager = new CreditManager(this);     // ➕ НОВОЕ
-        creditManager.load();                         // ➕ НОВОЕ
+        creditManager = new CreditManager(this);
+        creditManager.load();
 
-        passportManager = new PassportManager(this);  // ➕ НОВОЕ
-        passportManager.load();                       // ➕ НОВОЕ
+        passportManager = new PassportManager(this);
+        passportManager.load();
 
         taxTracker = new TaxTracker();
         freezeManager = new FreezeManager(this);
@@ -62,7 +56,7 @@ public class MinePlugin extends JavaPlugin {
         // GUI
         mineLevelGUI = new MineLevelGUI(this);
 
-        // Регистрация
+        // Регистрация слушателей
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(freezeManager, this);
         pm.registerEvents(mineLevelGUI, this);
@@ -75,7 +69,7 @@ public class MinePlugin extends JavaPlugin {
         // Запуск систем
         mineGenerator.start();
         scoreboardManager.start();
-        creditManager.startReminders();                // ➕ НОВОЕ
+        creditManager.startReminders();
 
         // Команды
         KaznaCommand kaznaCommand = new KaznaCommand(this);
@@ -88,18 +82,21 @@ public class MinePlugin extends JavaPlugin {
         getCommand("buy").setTabCompleter(shopCommand);
         pm.registerEvents(shopCommand, this);
 
-        CreditCommand creditCommand = new CreditCommand(this);  // ➕ НОВОЕ
-        getCommand("credit").setExecutor(creditCommand);         // ➕ НОВОЕ
-        getCommand("credit").setTabCompleter(creditCommand);     // ➕ НОВОЕ
+        CreditCommand creditCommand = new CreditCommand(this);
+        getCommand("credit").setExecutor(creditCommand);
+        getCommand("credit").setTabCompleter(creditCommand);
 
-        PassportCommand passportCommand = new PassportCommand(this);  // ➕ НОВОЕ
-        getCommand("passport").setExecutor(passportCommand);          // ➕ НОВОЕ
-        getCommand("passport").setTabCompleter(passportCommand);      // ➕ НОВОЕ
+        PassportCommand passportCommand = new PassportCommand(this);
+        getCommand("passport").setExecutor(passportCommand);
+        getCommand("passport").setTabCompleter(passportCommand);
+
+        // Команда перезагрузки конфига
+        getCommand("minereload").setExecutor(new ReloadCommand(this));
 
         getLogger().info("=================================");
-        getLogger().info("MinePlugin v3.0.0 загружен!");
-        getLogger().info("Шахта, казна, магазин, табло,");
-        getLogger().info("кредиты, паспорта.");
+        getLogger().info("MinePlugin v4.0.0 загружен!");
+        getLogger().info("Все настройки в config.yml");
+        getLogger().info("/minereload для перезагрузки");
         getLogger().info("=================================");
     }
 
@@ -115,13 +112,50 @@ public class MinePlugin extends JavaPlugin {
         getLogger().info("MinePlugin выгружен. Данные сохранены.");
     }
 
+    /**
+     * Перезагрузка конфига без перезапуска сервера
+     */
+    public void reloadPlugin() {
+        configManager.reload();
+        if (scoreboardManager != null) {
+            scoreboardManager.stop();
+            scoreboardManager.start();
+        }
+        if (mineGenerator != null) {
+            mineGenerator.start();
+        }
+        getLogger().info("Конфиг перезагружен!");
+    }
+
+    // Геттеры
     public static MinePlugin getInstance() { return instance; }
+    public ConfigManager getConfigManager() { return configManager; }
     public KaznaManager getKaznaManager() { return kaznaManager; }
     public MineLevelGUI getMineLevelGUI() { return mineLevelGUI; }
     public TaxTracker getTaxTracker() { return taxTracker; }
     public FreezeManager getFreezeManager() { return freezeManager; }
     public MineGenerator getMineGenerator() { return mineGenerator; }
     public ScoreboardManager getScoreboardManager() { return scoreboardManager; }
-    public CreditManager getCreditManager() { return creditManager; }         // ➕ НОВОЕ
-    public PassportManager getPassportManager() { return passportManager; }   // ➕ НОВОЕ
+    public CreditManager getCreditManager() { return creditManager; }
+    public PassportManager getPassportManager() { return passportManager; }
+
+    // === Внутренний класс команды /minereload ===
+    private static class ReloadCommand implements CommandExecutor {
+        private final MinePlugin plugin;
+        ReloadCommand(MinePlugin plugin) { this.plugin = plugin; }
+
+        @Override
+        public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
+                                  @NotNull String label, @NotNull String[] args) {
+            if (!sender.hasPermission("mine.admin")) {
+                sender.sendMessage(Component.text("Нет прав!")
+                        .color(NamedTextColor.RED));
+                return true;
+            }
+            plugin.reloadPlugin();
+            sender.sendMessage(Component.text("[MinePlugin] Конфиг перезагружен!")
+                    .color(NamedTextColor.GREEN));
+            return true;
+        }
+    }
 }
